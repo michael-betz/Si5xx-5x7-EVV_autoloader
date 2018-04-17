44f5d9d4c5deaf3aaeac4a23a6af2951053a91f9
Michael Betz <mbetz@lbl.gov>
2018-04-17 16:23:17 -0700

    complete setFreq.py. Tested with py2 and py3. Verified fOut with VSA

---
+++ Si570.py
@@ -0,0 +1,79 @@
+class Si570( object ):
+    """ 
+    helper class to convert from numbers used in the datasheet
+    to register values
+    """
+    HS_DIVS_LOOKUP = (4,5,6,7,None,9,None,11)
+    
+    def __init__(self, l=None):
+        """
+        l can be a string like 'r 01 C2 BC 81 83 02 \n'
+        """
+        if l:
+            ls = l.strip().split()[1:] # ['01', 'C2', 'BC', '81', '83', '02']
+            self._regs = bytearray([ int(x,16) for x in ls ])
+        else:
+            self._regs = bytearray(6);
+    
+    @property
+    def HS_DIV(self):
+        """ DCO High Speed Divider """
+        return Si570.HS_DIVS_LOOKUP[ self._regs[0] >> 5 ]
+    
+    @HS_DIV.setter
+    def HS_DIV(self, value):
+        ind = Si570.HS_DIVS_LOOKUP.index(value)
+        self._regs[0] &= 0x1F
+        self._regs[0] |= (ind<<5) & 0xE0
+    
+    @property
+    def N1(self):
+        """ CLKOUT Output Divider """
+        N1 = (self._regs[0]&0x1F)<<2 | self._regs[1]>>6
+        N1 += 1
+        # Illegal odd divider values will be rounded up to the nearest even value.
+        if( (N1 > 1) and (N1 & 0x01) ):
+            print( "Illegal N1: {0}, rounding up to {1}", N1, N1+1 )
+            N1 += 1
+        return N1
+    
+    @N1.setter
+    def N1(self, value):
+        value -= 1
+        self._regs[0] &= 0xE0
+        self._regs[0] |= (value>>2) & 0x1F
+        self._regs[1] &= 0x3F
+        self._regs[1] |= (value<<6) & 0xC0
+    
+    @property
+    def RFFREQ(self):
+        """ Reference Frequency control input to DCO. [float] """
+        RFFREQ = (self._regs[1]&0x3F)<<32 | self._regs[2]<<24 |         \
+                        self._regs[3]<<16 | self._regs[4]<<8  | self._regs[5]
+        return RFFREQ / 2.0**28
+    
+    @RFFREQ.setter
+    def RFFREQ(self, value):
+        value = int( value * 2**28 )
+        self._regs[1] &= 0xC0
+        self._regs[1] |= (value>>32) & 0x3F
+        self._regs[2]  = (value>>24) & 0xFF
+        self._regs[3]  = (value>>16) & 0xFF
+        self._regs[4]  = (value>> 8) & 0xFF
+        self._regs[5]  =  value      & 0xFF
+    
+    def fxtal(self, f0):
+        """ 
+        calculate internal crystal frequency. 
+        f0 = startup frequency (see http://www.silabs.com/products/timing/lookup-customize)
+        """
+        return ( f0 * self.HS_DIV * self.N1 ) / self.RFFREQ
+    
+    def __repr__(self):
+        s  = 'w '
+        s += ' '.join('{:02x}'.format(x) for x in self._regs)
+        return s
+    
+    def __str__(self):
+        s = "HS_DIV:{0:2d}, N1:{1:2d}, RFFREQ:{2:13.9f}".format( self.HS_DIV, self.N1, self.RFFREQ )
+        return s
--- setFreq.py
+++ setFreq.py
@@ -1,16 +1,17 @@
 """
 Configure a Si5xx-5x7-EVB board to a specific frequency.
-Board must run the `special` firmware.
+Board must run the `Si5xx-5x7-EVV_autoloader.hex` firmware
+and visible as a virtual USB serial port.
+
+Si570 datasheet:  https://www.silabs.com/documents/public/data-sheets/si570.pdf
 """
 import serial, argparse
-# from numpy import meshgrid, unravel_index
-
-HS_DIVS = [4,5,6,7,None,9,None,11]
-NS = [1]
-NS.extend( range(2,130,2) )
+from Si570 import Si570
 
 def getDividers( f1 ):
-    global HS_DIVS, NS
+    HS_DIVS = [4,5,6,7,None,9,None,11]
+    NS = [1]
+    NS.extend( range(2,130,2) )
     for h in HS_DIVS[::-1]:
         for n in NS:
             if h is None:
@@ -22,23 +23,21 @@ def getDividers( f1 ):
             if fDco>=4.85e9:
                 return h, n, fDco
     raise ValueError("Could not find a good combination of clock dividers")
-    # HH, NN = meshgrid( [ x for x in HS_DIVS if x ], NS )
-    # aDiffs = abs( args["f1"]*HH*NN - 4.85e9 )
-    # inds = unravel_index( aDiffs.argmin(), aDiffs.shape )
-    # return HS_DIVS[inds[0]], NS[inds[1]]
 
 def main():
     parser = argparse.ArgumentParser( description=__doc__ )
     parser.add_argument(
         'f0',
         type = float,
-        help =  "power-up frequency [Hz] of the Si570\
-                (see http://www.silabs.com/products/timing/lookup-customize)"
+        help =  "power-up frequency [Hz] of the Si570,\
+                 which corresponds to `Frequency A` on\
+                 http://www.silabs.com/products/timing/lookup-customize"
     )
     parser.add_argument(
         'f1',
         type = float,
-        help = "desired output frequency [Hz] (written to nonvolatile memory)"
+        help = "desired output frequency [Hz], which will be set on the device\
+                and also written to nonvolatile memory"
     )
     parser.add_argument(
         '-p', '--port',
@@ -46,52 +45,42 @@ def main():
         help = "serial port name"
     )
     args = parser.parse_args()
-    print(args)
 
+    print("Read initial register settings ... ")
     with serial.Serial( args.port, timeout=3 ) as ser:
-        ser.write(b'r')
-        l = ser.readline().decode()     # 'r 01 C2 BC 81 83 02 \n'
-    if not l.startswith('r '):
+        ser.write(b'i')
+        l = ser.readline().decode()     # 'i 01 C2 BC 81 83 02 \n'
+    if not l.startswith('i '):
         raise RuntimeError("Unexpected response: " + l)
-    print("Read: ", l.strip())
-    ls = l.strip().split()[1:]          # ['01', 'C2', 'BC', '81', '83', '02']
-    dats = [ int(x,16) for x in ls ]    # [1, 194, 188, 129, 131, 2]
 
-    HS_DIV = HS_DIVS[ dats[0] >> 5 ]
-    print("HS_DIV = ", HS_DIV)
-    N1 = (dats[0]&0x1F)<<2 | dats[1]>>6
-    N1 += 1
-    # Illegal odd divider values will be rounded up to the nearest even value.
-    if( (N1 > 1) and (N1 & 0x01) ):
-        N1 += 1
-    print("N1 = ", N1)
-    RFFREQ = (dats[1]&0x3F)<<32 | dats[2]<<24 |         \
-                    dats[3]<<16 | dats[4]<<8  | dats[5]
-    RFFREQ /= 2**28
-    print("RFFREQ = ", RFFREQ)
-
-    fxtal = ( args.f0 * HS_DIV * N1 ) / RFFREQ
-    print("fxtal = {:.6f} MHz".format(fxtal/1e6) )
+    # parse current settings
+    sil = Si570( l );
+    print("{:>24} --> {!s:<45}".format(l.strip(), sil) )
 
     # Calculate new settings
     hs_div, n1, fDco = getDividers( args.f1 )
-    rffreq = fDco / fxtal
+    rffreq = fDco / sil.fxtal( args.f0 )
 
-    # Convert to registers again
-    rffreq = int( rffreq * 2**28 )
-    sets = bytearray(6)
-    sets[5]  =  rffreq      & 0xFF
-    sets[4]  = (rffreq>> 8) & 0xFF
-    sets[3]  = (rffreq>>16) & 0xFF
-    sets[2]  = (rffreq>>24) & 0xFF
-    sets[1]  = (rffreq>>32) & 0xFF
-    n1 -= 1
-    sets[1] |= (    n1<< 6) & 0xFF
-    sets[0]  = (    n1>> 2)
-    hs_div = HS_DIVS.index( hs_div )
-    sets[0] |= (hs_div<< 5) & 0xFF
+    # Un-parse new settings
+    silNew = Si570();
+    silNew.HS_DIV = hs_div
+    silNew.N1 = n1
+    silNew.RFFREQ = rffreq
 
-    print( sets )
+    print("Calculate new settings ...")
+    print("{new!r:>24} --> {new!s:<45}".format(new=silNew) )
+
+    print("Write to device and flash ... ")
+    with serial.Serial( args.port, timeout=3 ) as ser:
+        ser.write( repr(silNew).encode("ascii") )
+        print(
+            "{:>24} --> {:}".format(
+                ser.readline().decode().strip(),
+                ser.readline().decode().strip()
+            )
+        )
+        ser.write( b"r" )
+        print( "{:>24}".format( ser.readline().decode().strip() ) )
 
 if __name__ == '__main__':
     main();
